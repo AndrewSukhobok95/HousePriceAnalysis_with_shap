ef05c6270e2df2fa1723e1aa2c84d7f01f3bfec9
U-ATREMA\asukhobok <asukhobok@RUASUKHOBOK.atrema.deloitte.com>
2019-03-22 18:09:53 +0300

    update

--- data_prep/custom_data_creating.py
+++ data_prep/custom_data_creating.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+from sklearn import model_selection, preprocessing
+
 
 def add_dates_info(df):
     def correct_str_month(date):
@@ -18,6 +22,110 @@ def add_dates_info(df):
 def add_price_per_sq_meter(df):
     df = df.copy()
     df["price_per_sq_meter"] = df["price_doc"] / df["full_sq"]
+    return df
+
+
+
+
+
+
+def create_custom_columns(df):
+    df = df.copy()
+    df["build_count_before_1945"] = df["build_count_before_1920"] + df["build_count_1921-1945"]
+    df["build_count_wood_slag"] = df['build_count_wood'] + df['build_count_slag']
+    df['female_male_diff'] = df['female_f'] - df['male_f']
+    df['young_female_male_diff'] = df['young_female'] - df['young_male']
+    df["edu_culture_service_km"] = df["kindergarten_km"] + df["preschool_km"] + \
+                                   df["school_km"] + df["university_km"] + df["additional_education_km"] + \
+                                   df["museum_km"] + df["exhibition_km"] + df["theater_km"]
+    df["energy_industry_km"] = df['incineration_km'] + df['nuclear_reactor_km'] + \
+                               df['radiation_km'] + df['power_transmission_line_km'] + df['thermal_power_plant_km']
+    df["fitness_service_km"] = df['fitness_km'] + df['swim_pool_km'] + \
+                               df['ice_rink_km'] + df['stadium_km'] + df['basketball_km']
+    df['school_preschool_raion'] = df['preschool_education_centers_raion'] + \
+                                   df['school_education_centers_raion']
+    return df[sorted(df.columns.tolist())]
+
+
+
+
+
+def prepare_choosed_features(train_df, cols, test_df=None, dont_touch_cols=[]):
+    def categorize_column(df, column_to_cat, lblencoder, drop_first_binary_feature=False):
+        df = df.copy()
+        cat_df = pd.DataFrame(lblencoder.transform(df[column_to_cat].astype(str)))
+        cat_df_cols = [column_to_cat + "_" + c for c in lblencoder.classes_]
+        if cat_df.shape[1] == 1:
+            cat_df_cols = cat_df_cols[0]
+            cat_df.columns = [cat_df_cols]
+        else:
+            cat_df.columns = cat_df_cols
+
+        if drop_first_binary_feature:
+            del cat_df[column_to_cat + "_" + lblencoder.classes_[0]]
+            cat_df_cols = cat_df_cols[1:]
+        del df[column_to_cat]
+        return pd.concat([df, cat_df], axis=1), cat_df_cols
+
+    new_cols = []
+    for col in cols:
+        # print("Processing column:", col)
+        if col in dont_touch_cols:
+            new_cols.append(col)
+        else:
+            if is_string_dtype(train_df[col]):
+                lbl = preprocessing.LabelBinarizer()
+                lbl.fit(train_df[col])
+                train_df, cat_columns = categorize_column(df=train_df, column_to_cat=col, lblencoder=lbl)
+                new_cols += [cat_columns] if type(cat_columns) == str else cat_columns
+                if test_df is not None:
+                    test_df, test_cat_columns = categorize_column(df=test_df, column_to_cat=col, lblencoder=lbl)
+            else:
+                new_cols.append(col)
+                if train_df[col].isnull().any():
+                    train_df[col] = train_df[col].fillna(train_df[col].median())
+    if test_df is not None:
+        return train_df[new_cols], test_df[new_cols]
+    else:
+        return train_df[new_cols]
+
+
+def manual_processing(df):
+    df = df.copy()
+
+    #     df["material0"] = df.material.isnull().astype(int)
+    df["material1"] = (df.material == 1).astype(int)
+    df["material2"] = (df.material == 2).astype(int)
+    df["material3"] = (df.material == 3).astype(int)
+    df["material4"] = (df.material == 4).astype(int)
+    df["material5"] = (df.material == 5).astype(int)
+    df["material6"] = (df.material == 6).astype(int)
+    # del df["material"]
+
+    df["build0"] = (df.build_year == 0).astype(int)
+    df["build1"] = (df.build_year == 1).astype(int)
+    df.loc[df.build_year > 2030, "build_year"] = np.NaN
+    df.loc[df.build_year < 1600, "build_year"] = np.NaN
+    df["nobuild"] = df.build_year.isnull().astype(int)
+    df["transaction_since_build"] = pd.to_datetime(df.timestamp).dt.year - df.build_year
+    df.build_year = df.build_year.fillna(df.build_year.mode().values[0])
+    df.transaction_since_build = df.transaction_since_build.fillna(df.transaction_since_build.median())
+
+    df["floor0"] = (df.floor == 0).astype(int)
+    df["floor1"] = (df.floor == 1).astype(int)
+    df["floorhuge"] = (df.floor > 40).astype(int)
+    # df["lnfloor"] = np.log(df.floor+1)
+
+    df["nomax_floor"] = df.max_floor.isnull().astype(int)
+    df.max_floor = df.max_floor.fillna(df.max_floor.median())
+    df["max0"] = (df.max_floor == 0).astype(int)
+    df["max1"] = (df.max_floor == 1).astype(int)
+    df["maxhuge"] = (df.max_floor > 80).astype(int)
+    # df["lnmax"] = np.log(df.max_floor+1)
+
+    del df["material"]
+    del df["timestamp"]
+
     return df
 
 
--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -3,10 +3,13 @@ import pandas as pd
 from pandas.api.types import is_string_dtype
 from pandas.api.types import is_numeric_dtype
 from sklearn import model_selection, preprocessing
-import xgboost as xgb
+# import xgboost as xgb
+from xgboost import XGBRegressor
 
 from data_prep.corrections import correct_macro_df, correct_flats_info_df
-from data_prep.custom_data_creating import add_dates_info
+from data_prep.custom_data_creating import add_dates_info, prepare_choosed_features, \
+    manual_processing, create_custom_columns
+from configs.columns import flats_param_columns, custom_flats_param_columns
 
 # import matplotlib.pyplot as plt
 # import seaborn as sns
@@ -20,11 +23,19 @@ from data_prep.custom_data_creating import add_dates_info
 # pd.set_option('display.max_rows', 500)
 
 
+USE_FEATURES = flats_param_columns + custom_flats_param_columns
+
 train_df = pd.read_csv("data/train.csv", parse_dates=['timestamp'])
 test_df = pd.read_csv("data/test.csv", parse_dates=['timestamp'])
 
 train_df = correct_flats_info_df(train_df)
 test_df = correct_flats_info_df(test_df)
+
+y_price = train_df.price_doc
+del train_df["price_doc"]
+
+train_df = create_custom_columns(train_df)
+test_df = create_custom_columns(test_df)
 
 macro_df = pd.read_csv("data/macro.csv", parse_dates=['timestamp'])
 macro_df = correct_macro_df(macro_df=macro_df)
@@ -39,9 +50,46 @@ test_with_macro_df = add_dates_info(test_with_macro_df)
 
 
 
+train_df_processed, test_df_processed = prepare_choosed_features(
+    train_with_macro_df, USE_FEATURES, test_df=test_with_macro_df,
+    dont_touch_cols=["build_year", "timestamp", "material", "max_floor", "timestamp_year_month"])
+
+train_df_processed = manual_processing(train_df_processed)
+test_df_processed = manual_processing(test_df_processed)
+
+
+y_price.apply(np.log1p)
+
+
+def cross_val_split(train_df, n_folds, fold_size):
+    sorted_dates_str = sorted(train_df.timestamp_year_month.unique())
+
+    for i in range(n_folds):
+        cur_fold_dates = sorted_dates_str[-fold_size-i:len(sorted_dates_str)-i]
 
 
 
 
 
 
+
+def rmsle_exp(y_true_log1p, y_pred_log1p):
+    y_true = np.expm1(y_true_log1p)
+    y_pred = np.expm1(y_pred_log1p)
+    return np.sqrt(np.mean(np.power(np.log(y_true + 1) - np.log(y_pred + 1), 2)))
+
+
+# X = train_df_processed.copy()
+# del X["timestamp"]
+# Y = y_price.apply(np.log)
+#
+# model = XGBRegressor()
+#
+# for (train, test), i in zip(cv.split(X, Y), range(5)):
+#     model.fit(X.iloc[train], Y.iloc[train])
+#     pred_train = model.predict(X.iloc[train])
+#     pred_test = model.predict(X.iloc[test])
+
+
+
+print()
