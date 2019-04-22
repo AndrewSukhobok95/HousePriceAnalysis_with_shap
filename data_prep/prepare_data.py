327fa7eb67da0252dd334f88166bff254d963a9a
andrey <suha1253516@gmail.com>
2019-04-22 12:39:31 +0300

    one_model_with_macro_with_corr_elimination_direction

--- data_prep/prepare_data.py
+++ data_prep/prepare_data.py
@@ -1,20 +1,12 @@
 import numpy as np
 import pandas as pd
-import xgboost as xgb
-from xgboost import XGBRegressor
-import pickle
-import json
-from datetime import datetime
 
 from data_prep.corrections import correct_macro_df, correct_flats_info_df
-from data_prep.custom_data_creating import add_dates_info, prepare_choosed_features, \
-    manual_processing, create_custom_columns
-from pred_utils.pred_utils import rmsle_exp, cross_val_split
-from configs.columns import flats_param_columns, custom_flats_param_columns
+from data_prep.custom_data_creating import add_dates_info, prepare_choosed_features, create_custom_columns
+#, manual_processing
 
-USE_FEATURES = flats_param_columns + custom_flats_param_columns
-
-def prepare_data(train_df, test_df, macro_df):
+def prepare_data(train_df, test_df, macro_df, USE_FEATURES,
+                 dont_touch_cols=["build_year", "timestamp", "material", "max_floor", "timestamp_year_month"]):
     train_df = correct_flats_info_df(train_df)
     test_df = correct_flats_info_df(test_df)
 
@@ -24,7 +16,6 @@ def prepare_data(train_df, test_df, macro_df):
 
     train_df = create_custom_columns(train_df)
     test_df = create_custom_columns(test_df)
-
 
     macro_df = correct_macro_df(macro_df=macro_df)
     macro_df.columns = ["timestamp"] + ["macro_" + c for c in macro_df.columns if c!="timestamp"]
@@ -37,10 +28,7 @@ def prepare_data(train_df, test_df, macro_df):
 
     train_df_processed, test_df_processed = prepare_choosed_features(
         train_with_macro_df, USE_FEATURES, test_df=test_with_macro_df,
-        dont_touch_cols=["build_year", "timestamp", "material", "max_floor", "timestamp_year_month"])
-
-    train_df_processed = manual_processing(train_df_processed)
-    test_df_processed = manual_processing(test_df_processed)
+        dont_touch_cols=dont_touch_cols)
 
     return train_df_processed, test_df_processed, Y_log1p
 
@@ -50,4 +38,4 @@ if __name__=="__main__":
     test_df = pd.read_csv("data/test.csv", parse_dates=['timestamp'])
     macro_df = pd.read_csv("data/macro.csv", parse_dates=['timestamp'])
 
-    train_df_processed, test_df_processed, Y_log1p = prepare_data(train_df, test_df, macro_df)
+    # train_df_processed, test_df_processed, Y_log1p = prepare_data(train_df, test_df, macro_df)
--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import xgboost as xgb
@@ -7,15 +8,24 @@ import json
 from datetime import datetime
 
 from pred_utils.pred_utils import rmsle_exp, cross_val_split
-from configs.columns import flats_param_columns, custom_flats_param_columns
-from data_prep.prepare_data import prepare_data, USE_FEATURES
+from data_prep.prepare_data import prepare_data
 
-train_df = pd.read_csv("data/train.csv", parse_dates=['timestamp'])
-test_df = pd.read_csv("data/test.csv", parse_dates=['timestamp'])
-macro_df = pd.read_csv("data/macro.csv", parse_dates=['timestamp'])
+from prediction_branches.one_model_with_macro.configs.columns import flats_param_columns, \
+    custom_flats_param_columns, macro_cols
+from prediction_branches.one_model_with_macro.custom_funcs.funcs import manual_processing
 
+train_df = pd.read_csv("../../data/train.csv", parse_dates=['timestamp'])
+test_df = pd.read_csv("../../data/test.csv", parse_dates=['timestamp'])
+macro_df = pd.read_csv("../../data/macro.csv", parse_dates=['timestamp'])
 
-train_df_processed, test_df_processed, Y_log1p = prepare_data(train_df, test_df, macro_df)
+USE_FEATURES = flats_param_columns + custom_flats_param_columns + macro_cols
+train_df_processed, test_df_processed, Y_log1p = prepare_data(
+    train_df, test_df, macro_df, USE_FEATURES,
+    dont_touch_cols=["build_year", "timestamp", "material", "max_floor", "timestamp_year_month"]
+)
+train_df_processed = manual_processing(train_df_processed)
+test_df_processed = manual_processing(test_df_processed)
+
 
 
 train_test_index_list = cross_val_split(train_df_processed, 5, 3)
@@ -39,15 +49,15 @@ for (train, val), i in zip(train_test_index_list, range(5)):
 
 model.fit(X, Y_log1p)
 
-model_comment = "without_macro"
+model_comment = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 now_time = datetime.now()
 now_time_str = now_time.strftime("%Y_%m_%d_%H_%M")
 model_name = "xgb_{}_model_{}_{}".format(xgb.__version__, now_time_str, model_comment)
-pickle.dump(model, open("trained_models/" + model_name + ".dat", "wb"))
+pickle.dump(model, open("../trained_models/" + model_name + ".dat", "wb"))
 
 json_features_data = {'features_names': USE_FEATURES}
 # jstr = json.dumps(json_features_data, indent=4)
-with open("trained_models/features_json/" + model_name + '.json', 'w') as outfile:
+with open("../trained_models/features_json/" + model_name + '.json', 'w') as outfile:
     json.dump(json_features_data, outfile)
 
 pred_test_log1p = model.predict(X_test)
